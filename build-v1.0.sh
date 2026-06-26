927c28ac7b5ba2c740f67665ccc28f2b409f7e78
Vladimir Petroukhin <v.petroukhin@gmail.com>
2026-06-26 21:14:54 +0300

    EasyMaps v1.1.1 cleanup: remove legacy files and structure

--- build-v1.0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE=/Users/petroukhinmac/Garmin/RoadDay
-
-java -Xmx6G -jar /Users/petroukhinmac/Garmin/mkgmap/mkgmap-r4924/mkgmap.jar \
-  --gmapsupp \
-  --route \
-  --index \
-  $BASE/osm/*.osm.pbf
